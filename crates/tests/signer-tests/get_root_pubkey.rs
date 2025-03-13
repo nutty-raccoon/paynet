86287cbd5dd5b69157db9a36e32a9f5f3f83dca0
Gerson2102 <gersonloavas@gmail.com>
2025-03-13 14:37:51 -0600

    Adding tests function of getting pubkey value

--- crates/tests/signer-tests/get_root_pubkey.rs
+++ crates/tests/signer-tests/get_root_pubkey.rs
@@ -4,12 +4,37 @@ use signer_tests::init_signer_client;
 
 #[tokio::test]
 async fn get_root_pubkey() -> Result<()> {
+    #[cfg(debug_assertions)]
+    {
+        let _ = dotenvy::from_filename("signer.env")
+            .inspect_err(|e| println!("dotenvy initialization failed: {e}"));
+    }
+
     let mut client = init_signer_client().await?;
     let res = client.get_root_pub_key(GetRootPubKeyRequest {}).await?;
     let get_root_pubkey_response: GetRootPubKeyResponse = res.into_inner();
     assert_eq!(
         get_root_pubkey_response.root_pubkey,
-        "0x0000000000000000000000000000000000000000000000000000000000000000"
+        "03915919cf8c316d50424df508c2b64d8e3d1ea7d55bbb6832df9b931cdfbcedd5"
+    );
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn get_root_pubkey_non_equal() -> Result<()> {
+    #[cfg(debug_assertions)]
+    {
+        let _ = dotenvy::from_filename("signer.env")
+            .inspect_err(|e| println!("dotenvy initialization failed: {e}"));
+    }
+
+    let mut client = init_signer_client().await?;
+    let res = client.get_root_pub_key(GetRootPubKeyRequest {}).await?;
+    let get_root_pubkey_response: GetRootPubKeyResponse = res.into_inner();
+    assert_ne!(
+        get_root_pubkey_response.root_pubkey,
+        "03915919cf8c316d50424df508c2b64d8e3d1ea7d55bbb6832df9b931cdfbcedd4"
     );
 
     Ok(())
