6c6c1dbb2b684c7a288c549eb7a17c0be8482c40
Gerson2102 <gersonloavas@gmail.com>
2025-03-15 11:58:18 -0600

    Addressing PR comments

--- crates/tests/signer-tests/get_root_pubkey.rs
+++ crates/tests/signer-tests/get_root_pubkey.rs
@@ -4,12 +4,6 @@ use signer_tests::init_signer_client;
 
 #[tokio::test]
 async fn get_root_pubkey() -> Result<()> {
-    #[cfg(debug_assertions)]
-    {
-        let _ = dotenvy::from_filename("signer.env")
-            .inspect_err(|e| println!("dotenvy initialization failed: {e}"));
-    }
-
     let mut client = init_signer_client().await?;
     let res = client.get_root_pub_key(GetRootPubKeyRequest {}).await?;
     let get_root_pubkey_response: GetRootPubKeyResponse = res.into_inner();
@@ -23,12 +17,6 @@ async fn get_root_pubkey() -> Result<()> {
 
 #[tokio::test]
 async fn get_root_pubkey_non_equal() -> Result<()> {
-    #[cfg(debug_assertions)]
-    {
-        let _ = dotenvy::from_filename("signer.env")
-            .inspect_err(|e| println!("dotenvy initialization failed: {e}"));
-    }
-
     let mut client = init_signer_client().await?;
     let res = client.get_root_pub_key(GetRootPubKeyRequest {}).await?;
     let get_root_pubkey_response: GetRootPubKeyResponse = res.into_inner();
