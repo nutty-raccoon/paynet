470ea1980919899ada0dda33b69eefe110bb8211
Gerson2102 <gersonloavas@gmail.com>
2025-03-18 13:08:03 -0600

    Deleting second test

--- crates/tests/signer-tests/get_root_pubkey.rs
+++ crates/tests/signer-tests/get_root_pubkey.rs
@@ -26,26 +26,3 @@ async fn get_root_pubkey() -> Result<()> {
 
     Ok(())
 }
-
-#[tokio::test]
-async fn get_root_pubkey_non_equal() -> Result<()> {
-    let mut client = init_signer_client().await?;
-    let res = client.get_root_pub_key(GetRootPubKeyRequest {}).await?;
-    let get_root_pubkey_response: GetRootPubKeyResponse = res.into_inner();
-
-    let root_key = env::var("ROOT_KEY").expect("ROOT_KEY must be set");
-    let xpriv = Xpriv::from_str(&root_key).expect("Invalid private key");
-
-    let secp256k1 = Secp256k1::new();
-    let pubkey = xpriv.private_key.public_key(&secp256k1);
-
-    let pubkey_hex = pubkey.to_string();
-    let pubkey = PublicKey::from_str(&pubkey_hex).expect("Invalid public key hex");
-    
-    assert_ne!(
-        get_root_pubkey_response.root_pubkey,
-        pubkey.to_string().replace("4", "1")
-    );
-
-    Ok(())
-}
