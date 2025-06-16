6fc0502ceb30fa7bcd9a200e17054c7e291ffd51
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2025-06-16 21:17:57 +0200

    feat: display total balance

--- mobile/salto/src/utils.ts
+++ mobile/salto/src/utils.ts
@@ -31,3 +31,18 @@ export function increaseNodeBalance(nodes: NodeData[], balanceChange: BalanceInc
         console.log("error: deposited on a node not registered in the state");
       }
 }
+
+export function computeTotalBalancePerUnit(nodes: NodeData[]): Map<string, number> {
+  const map: Map<string, number> = new Map();
+  nodes.forEach((n) => n.balances.forEach((b) => {
+    let currentAmount = map.get(b.unit);
+    if (!!currentAmount) {
+      map.set(b.unit, currentAmount + b.amount);
+    } else {
+      map.set(b.unit, b.amount);
+    }
+  }))
+
+
+  return map;
+}
