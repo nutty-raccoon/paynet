916b5bdcbfadff496dd66f00ed84ccbdc15f753e
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2025-06-17 02:10:19 +0200

    feat: add create and receive wad commands

--- mobile/salto/src-tauri/src/commands/mod.rs
+++ mobile/salto/src-tauri/src/commands/mod.rs
@@ -1,7 +1,17 @@
 mod add_node;
 mod deposit;
 mod get_nodes_balance;
+mod wad;
 
 pub use add_node::add_node;
 pub use deposit::{create_mint_quote, redeem_quote};
 pub use get_nodes_balance::get_nodes_balance;
+pub use wad::{create_wad, receive_wad};
+
+#[derive(Clone, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BalanceIncrease {
+    node_id: u32,
+    unit: String,
+    amount: u64,
+}
