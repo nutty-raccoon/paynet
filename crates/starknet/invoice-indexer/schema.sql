270169c6146f60cd205efb4d2ae5e56fd3184db6
Tuan Tran <tropicaldog17@gmail.com>
2025-07-09 10:47:05 +0000

    add schema

---
+++ crates/starknet/invoice-indexer/schema.sql
@@ -0,0 +1,12 @@
+ CREATE TABLE IF NOT EXISTS invoice_contract_events (
+            id TEXT PRIMARY KEY,
+            block_id TEXT NOT NULL,
+            tx_hash TEXT NOT NULL,
+            event_index INTEGER NOT NULL,
+            payee TEXT NOT NULL,
+            asset TEXT NOT NULL,
+            invoice_id TEXT NOT NULL,
+            payer TEXT NOT NULL,
+            amount_low TEXT NOT NULL,
+            amount_high TEXT NOT NULL
+        );
