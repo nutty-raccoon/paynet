704ad71a7aa2e6da3832c867b4b91da7155a6b12
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2025-09-11 17:24:58 -0300

    tmp

--- docker-compose/prod/init-letsencrypt.sh
+++ docker-compose/prod/init-letsencrypt.sh
@@ -8,14 +8,14 @@ fi
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export COMPOSE_FILE="$SCRIPT_DIR/node-starknet-sepolia.yaml"
 
-domains=("$DOMAIN_NAME")
+domain="$DOMAIN_NAME"
 rsa_key_size=4096
-data_path="./data/certbot"
-email="tdelabro@gmail.com" 
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+data_path="$SCRIPT_DIR/data/certbot"
+email="tdelabro@gmail.com"
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+  read -p "Existing data found for $domain. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
     exit
   fi
@@ -30,9 +30,9 @@ if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/
   echo
 fi
 
-echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
+echo "### Creating dummy certificate for $domain ..."
+path="/etc/letsencrypt/live/$domain"
+mkdir -p "$data_path/conf/live/$domain"
 docker compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
@@ -45,20 +45,15 @@ echo "### Starting nginx ..."
 docker compose up --force-recreate -d nginx
 echo
 
-echo "### Deleting dummy certificate for $domains ..."
+echo "### Deleting dummy certificate for $domain ..."
 docker compose run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+  rm -Rf /etc/letsencrypt/live/$domain && \
+  rm -Rf /etc/letsencrypt/archive/$domain && \
+  rm -Rf /etc/letsencrypt/renewal/$domain.conf" certbot
 echo
 
 
-echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
-domain_args=""
-for domain in "${domains[@]}"; do
-  domain_args="$domain_args -d $domain"
-done
+echo "### Requesting Let's Encrypt certificate for $domain ..."
 
 # Select appropriate email arg
 case "$email" in
@@ -70,10 +65,10 @@ esac
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
 docker compose run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
+  certbot -v certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
-    $domain_args \
+    -d $domain \
     --rsa-key-size $rsa_key_size \
     --agree-tos \
     --force-renewal" certbot
