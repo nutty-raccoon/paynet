c2e9b15155e95a27696be86cef0d1756046961f1
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2025-09-14 00:45:38 -0300

    feat(node): add tonic reflexion

--- crates/bins/node/build.rs
+++ crates/bins/node/build.rs
@@ -1,5 +1,9 @@
+use std::{env, path::PathBuf};
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("node_service_desciptor.bin"))
         .build_client(false)
         .build_server(true)
         .compile_protos(
